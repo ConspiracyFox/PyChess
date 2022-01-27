70ccad9128e00d6963021e62c0b0b3c5cc687244
Thomas S <90867261+MentorDerGrosse@users.noreply.github.com>
2022-01-27 10:59:20 +0100

    Update sicilian.py

--- openings/sicilian.py
+++ openings/sicilian.py
@@ -1,30 +1,49 @@
 import chess
+import time
 
 board = chess.Board()
 
+time.sleep(2.5)
+
 board.push_san("e4")
 print(board)
+time.sleep(2.5)
+
 print("\n")
 board.push_san("c5")
 print(board)
+time.sleep(2.5)
+
 print("\n")
 board.push_san("Nf3")
 print(board)
+time.sleep(2.5)
+
 print("\n")
 board.push_san("d6")
 print(board)
+time.sleep(2.5)
+
 print("\n")
 board.push_san("d4")
 print(board)
+time.sleep(2.5)
+
 print("\n")
 board.push_san("c5xd4")
 print(board)
+time.sleep(2.5)
+
 print("\n")
 board.push_san("Nd4")
 print(board)
+time.sleep(2.5)
+
 print("\n")
 board.push_san("Nf6")
 print(board)
+time.sleep(2.5)
+
 print("\n")
 board.push_san("Nc3")
 print(board)
