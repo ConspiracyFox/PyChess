5f60db1fb4ca62318934404e35ece2f08df964ed
MentorDerGrosse <90867261+MentorDerGrosse@users.noreply.github.com>
2022-01-27 01:35:28 +0100

    Update pychess.py

--- pychess.py
+++ pychess.py
@@ -1,19 +1,35 @@
 import chess
 
-
 board=chess.Board()
+
 i=0
+
 print(board)
 
 while (i<100):
+
     a=input()
+
     try:
+
         board.push_san(a)
+
     except:
+
         print("An error accoured")
-        break
+
     print(board)
+
     b=input()
-    board.push_san(b)
+
+    try:
+
+        board.push_san(b)
+
+    except:
+
+        print("An error accoured")
+
     print(board)
+
     i= i+1
